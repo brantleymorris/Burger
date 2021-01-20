9bc6ded76586ec46c2e4a50d14f77d81c944ed5a
BPM <brantleymorris@me.com>
2021-01-20 15:48:03 -0600

    add jawsdb for deploying server

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,17 @@
 const mysql = require("mysql2");
+var connection;
 
-const connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "password",
-    database: "burgers_db"
-});
+if (process.env.JAWSDB_DURL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "password",
+        database: "burgers_db"
+    });
+}
 
 connection.connect(function(err) {
     if (err) {
